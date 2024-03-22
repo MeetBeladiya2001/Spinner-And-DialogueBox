15a1e4b5307405dc08ad5327052072872b4a5878
meet555 <meetbeladiya@gmail.com>
2024-03-22 09:41:48 +0530

    First

--- app/src/main/java/com/example/crudapp/MainActivity.kt
+++ app/src/main/java/com/example/crudapp/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 val selectedItem = parent?.getItemAtPosition(position).toString()
                 Toast.makeText(this@MainActivity,"You Have Selected $selectedItem",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity,"You Have Selected $selectedItem",Toast.LENGTH_SHORT).show()
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
